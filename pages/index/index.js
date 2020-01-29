10b437c92ecc71ed0da9e32bb6189d95dcaa2db3
蟑螂恶霸 <zhanglangeba@qq.com>
2020-01-29 11:25:53 +0800

    轮播图渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,62 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 轮播图数据
+    swiperData: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+
   onLoad: function (options) {
-
+    this.getSwiperData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 获取轮播图数据
+  getSwiperData() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+        // console.log(result);
+        this.setData({
+          swiperData:result.data.message
+        })
+      },
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    });
 
   }
+
 })
