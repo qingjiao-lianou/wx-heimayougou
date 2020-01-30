04f0f2fb5a20f644985dd91495b1ee70d3b41eb1
蟑螂恶霸 <zhanglangeba@qq.com>
2020-01-30 14:21:54 +0800

    商品列表渲染

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,4 @@
-// pages/goods_list/index.js
+import { request } from '../../request/index'
 Page({
 
   /**
@@ -12,18 +12,41 @@ Page({
       { id: 2, text: "价格" }
     ],
     currentIndex: 0,//当前索引
+    goodsList:[], //商品列表
+  },
 
+  //请求商品列表参数
+  listParams: {
+    query: '',
+    cid: '',
+    pagesize: 20,
+    pagenum: 1
   },
 
 
   onLoad: function (options) {
-
+    this.listParams.cid = options.cid
+    this.getGoodsList()
   },
 
-  handleItem(e){
-   const {index} = e.detail
-   this.setData({
-    currentIndex:index
+  // 切换导航
+  handleItem(e) {
+    const { index } = e.detail
+    this.setData({
+      currentIndex: index
+    })
+  },
+
+  // 获取商品列表数据
+  getGoodsList(){
+   request({
+     url:"/goods/search",
+     params:this.listParams
+   }).then(res => {
+     this.setData({
+       goodsList:res.data.message.goods
+     })
+     
    })
   }
 
