ae03cc22538714b4aa50a49a02785b35387c95d2
蟑螂恶霸 <zhanglangeba@qq.com>
2020-01-30 16:49:01 +0800

    滚动条触底事件的实现

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -12,17 +12,19 @@ Page({
       { id: 2, text: "价格" }
     ],
     currentIndex: 0,//当前索引
-    goodsList:[], //商品列表
+    goodsList: [], //商品列表
   },
 
   //请求商品列表参数
   listParams: {
     query: '',
     cid: '',
-    pagesize: 20,
+    pagesize: 10,
     pagenum: 1
   },
 
+  // 总页数
+  sumPagenum: 1,
 
   onLoad: function (options) {
     this.listParams.cid = options.cid
@@ -38,16 +40,37 @@ Page({
   },
 
   // 获取商品列表数据
-  getGoodsList(){
-   request({
-     url:"/goods/search",
-     params:this.listParams
-   }).then(res => {
-     this.setData({
-       goodsList:res.data.message.goods
-     })
-     
-   })
+  getGoodsList() {
+    request({
+      url: "/goods/search",
+      data: this.listParams
+    }).then(res => {
+      // 计算数据总页数
+      this.sumPagenum = Math.ceil(res.data.message.total / this.listParams.pagesize)
+      // 之前的数据
+      const oldGoodsList = this.data.goodsList
+      // 新的数据
+      const newGoodsList = res.data.message.goods
+      this.setData({
+        goodsList: [...oldGoodsList, ...newGoodsList]
+      })
+
+    })
+  },
+
+  // 滚动条触底事件
+  onReachBottom() {
+    if (this.listParams.pagenum >= this.sumPagenum) {
+      wx.showToast({
+        title: '无',
+        icon: 'none',
+      });
+    } else {
+      // 有下一页数据
+      this.listParams.pagenum++;
+      this.getGoodsList()
+    }
+
   }
 
 })
