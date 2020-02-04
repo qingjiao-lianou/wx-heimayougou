87f45b033111bb2f5c7dc3074bc436c3fa134c58
蟑螂恶霸 <zhanglangeba@qq.com>
2020-02-04 14:52:29 +0800

    实现加入购物车功能

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -36,6 +36,31 @@ Page({
       urls
     });
 
+  },
+
+  // 点击加入购物车
+  addCarts() {
+    let cartList = wx.getStorageSync('carts') || [];
+    const { goodsInfo } = this.data
+    const index = cartList.findIndex(v => v.goods_id === goodsInfo.goods_id)
+    if (index === -1) {
+      //  如果没有
+      cartList.push({
+        goods_id: goodsInfo.goods_id,
+        goods_name: goodsInfo.goods_name,
+        goods_price: goodsInfo.goods_price,
+        goods_small_logo: goodsInfo.goods_small_logo,
+        num: 1
+      })
+    } else {
+      cartList[index].num++
+    }
+    wx.setStorageSync('carts', cartList);
+    wx.showToast({
+      title: '加入成功',
+      mask: true,
+    });
+      
   }
 
 })
